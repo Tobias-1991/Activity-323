17c2ec960ba8b2e1e4fe0ad081ee60c6bab6dbcf
Tobias-1991 <62919937+Tobias-1991@users.noreply.github.com>
2021-08-15 22:59:52 -0700

    Update DatabaseConnection.php

--- DatabaseConnection.php
+++ DatabaseConnection.php
@@ -4,15 +4,19 @@ class DatabaseConnection
 {
     function getConnection() {
         
-        $servername = "z5zm8hebixwywy9d.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+        $url = getenv('z5zm8hebixwywy9d.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+        $dbparts = parse_url($url);
         
-        $username = "hydassi67brqr6xz";
+        $hostname = $dbparts['z5zm8hebixwywy9d.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
+        $username = $dbparts['hydassi67brqr6xz'];
+        $password = $dbparts['x2oksfqgyajdcw9l'];
+        $database = ltrim($dbparts['fsu8e516wvoz7kvx'],'/');
         
-        $password = "x2oksfqgyajdcw9l";
         
-        $database = "fsu8e516wvoz7kvx";
         
-        $conn = new mysqli($servername, $username, $password, $database);
+   
+        
+        $conn = new mysqli($hostname, $username, $password, $database);
         
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
